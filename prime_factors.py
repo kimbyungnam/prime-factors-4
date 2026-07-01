6724a6f8c0816515dfd896314e8cd3c31135c3c8
kimbyungnam <kimbyungnam@ajou.ac.kr>
2026-07-01 12:59:26 +0900

    [refactoring] 6

--- prime_factors.py
+++ prime_factors.py
@@ -3,20 +3,13 @@ class PrimeFactor:
         factors = []
         if num > 1:
             divisor = 2
-            if num == 4:
-                while num % divisor == 0:
-                    factors.append(divisor)
-                    num //= divisor
-            elif num == 6:
+            if num == 4 or num == 6 or num == 9:
                 divisor = 2
                 while num > 1:
                     while num % divisor == 0:
                         factors.append(divisor)
                         num //= divisor
                     divisor += 1
-            elif num == 9:
-                factors.append(3)
-                factors.append(3)
             else:
                 factors.append(num)
         return factors
