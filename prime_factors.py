e77376184a4bd5606c49a275bd90a8d6872d3ba8
kimbyungnam <kimbyungnam@ajou.ac.kr>
2026-07-01 12:51:04 +0900

    [feature] 4

--- prime_factors.py
+++ prime_factors.py
@@ -1,5 +1,7 @@
 class PrimeFactor:
     def of(self, num: int) -> list[int]:
         if num > 1:
+            if num == 4:
+                return [2, 2]
             return [num]
         return []
